edbcd811175eb7eaff6083ee659091192ae9d8e3
Samar AYADI <ayadisamardev@gmail.com>
2024-08-26 20:56:34 +0100

    JWT Token Verification & Middleware

--- server/controllers/AuthController.js
+++ server/controllers/AuthController.js
@@ -75,3 +75,26 @@ export const login = async (req, res, next) => {
     return res.status(500).send("Internal Server Error");
   }
 };
+
+
+export const  getUserInfo = async (request, response, next) => {
+    try {
+    const userData = await User.findById(request.userId);
+    if(!userData) {
+        return response.status(404).send("User with the given id not found");
+    }
+  
+        return response.status(200).json({
+            id: userData.id,
+            email: userData.email,
+            profileSetup: userData.profileSetup,
+            firstName: userData.firstName,
+            lastName: userData.lastName,
+            image: userData.image,
+            color: userData.color,
+        });
+    } catch (error) {
+      console.log(error);
+      return response.status(500).send("Internal Server Error");
+    }
+  };
